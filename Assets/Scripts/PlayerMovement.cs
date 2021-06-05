227f76ef79eeb3edc046172dc7a2f321fa1cc2f6
BhumeshwariKhotele <bhumeshwari.khotele@xcubelabs.com>
2021-06-05 04:45:39 -0700

    Changes made in Player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,22 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
-
-
 {
 
-    public float playerSpeed;
-    Vector3 direction;
+    public float PlayerSpeed;
+    public Player localPlayer;
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        direction = Vector3.forward;
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.Translate(direction * playerSpeed * Time.deltaTime);
-
+        float horizantal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        rb.velocity = new Vector2(horizantal, vertical) * PlayerSpeed;
+        rb.position = new Vector3(Mathf.Clamp(rb.position.x, localPlayer.xMin, localPlayer.xMax), Mathf.Clamp(rb.position.y, localPlayer.yMin, localPlayer.yMax));
     }
+}
+
+[System.Serializable]
+public class Player
+{
+    public float xMin, xMax, yMin, yMax;
 }
---
+++ Assets/SpaceSkies Free/Demo/Scripts/SkyboxChanger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SkyboxChanger : MonoBehaviour
+{
+    public Material[] Skyboxes;
+    private Dropdown _dropdown;
+    private float rotateSpeed = 3.0f;
+    public void Awake()
+    {
+        _dropdown = GetComponent<Dropdown>();
+        //var options = Skyboxes.Select(skybox => skybox.name).ToList();
+        //_dropdown.AddOptions(options);
+    }
+    private void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * rotateSpeed);
+    }
+    public void ChangeSkybox()
+    {
+        RenderSettings.skybox = Skyboxes[_dropdown.value];
+    }
+}
