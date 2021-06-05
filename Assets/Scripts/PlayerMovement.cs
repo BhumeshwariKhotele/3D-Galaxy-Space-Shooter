3a199d58d344a4123651331bb55378d1285f6869
BhumeshwariKhotele <bhumeshwari.khotele@xcubelabs.com>
2021-06-05 01:06:36 -0700

    Added Player movement and space environment

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
+
+
 {
+
+    public float playerSpeed;
+    Vector3 direction;
     // Start is called before the first frame update
     void Start()
     {
-        
+        direction = Vector3.forward;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(direction * playerSpeed * Time.deltaTime);
+
     }
 }
